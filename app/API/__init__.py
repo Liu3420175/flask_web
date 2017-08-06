326177c118b8e57a555780c514ac2a5cc1ab639e
liulonghua <1196201660@qq.com>
2017-08-06 09:55:36 +0800

    liulonghua

--- app/API/__init__.py
+++ app/API/__init__.py
@@ -1,3 +1,6 @@
-"""
-内部接口
-"""
+from .view import  UserApi, PayUnifiedorderApi, CityApi, SearchSchedulesApi, \
+         WXLoginApi, WXPayApi, LocationApi, OrderDetailApi, RouteApi, UserOrderApi, \
+         PassengerApi
+
+__all__ = ["UserOrderApi","UserApi","PassengerApi","PayUnifiedorderApi","WXPayApi","RouteApi",
+           "WXLoginApi","CityApi","OrderDetailApi","LocationApi","SearchSchedulesApi"]
