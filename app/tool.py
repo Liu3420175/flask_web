ff1f10472335959c1dbed08af49be27f4958a19e
liulonghua <1196201660@qq.com>
2017-08-09 14:36:18 +0800

    liulonghua

--- app/tool.py
+++ app/tool.py
@@ -72,3 +72,57 @@ def seconds_2_time(seconds):
     return ":".join([hour_str,minute_str])
 
 
+
+def sendEmail(_to=(),subject="ABC",file_path=("/home/abc/efd.pdf",)):
+    """
+    发送邮件
+    :return: 
+    """
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.application import MIMEApplication
+    from collections import Iterable
+    import os
+
+    username = 'XXXXX@qq.com'
+    password = 'XXXXXXXX'
+    sender = username
+    if isinstance(_to,Iterable) and not isinstance(_to,str) and _to:
+        #判断其是不是可迭代对象,但不是字符串
+        receivers = ','.join(_to)
+    else:
+        raise TypeError()
+
+    # 如名字所示： Multipart就是多个部分
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receivers
+
+    # 下面是文字部分
+    puretext = MIMEText('Hello World')
+    msg.attach(puretext)
+
+    #发送附件
+    if isinstance(file_path,(list,tuple)):
+        for filename in file_path:
+            if os.path.exists(filename):
+                suffix_name = filename.split(".")[-1]#后缀名
+                attachment = MIMEApplication(open(filename,"rb").read(),
+                                             _subtype=suffix_name)
+                attachment.add_header("Content-Disposition",
+                                      "attachment",
+                                      filename="abc.{0}".format(suffix_name))
+                msg.attach(attachment)
+
+    ##  下面开始真正的发送邮件了
+    try:
+        client = smtplib.SMTP()
+        client.connect('https://mail.qq.com')
+        client.login(username, password)
+        client.sendmail(sender, receivers, msg.as_string())
+        client.quit()
+        print('邮件发送成功！')
+    except Exception as e:
+        print("发送失败,{0}".format(str(e)))
